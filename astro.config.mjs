5944fe2b51eae2b0d4c15a4f9c084cf776720760
Julian Simon <juedsay@gmail.com>
2025-01-23 04:08:16 -0300

    solucionando errores para el deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
+import vercel from "@astrojs/adapter-vercel";
 import preact from "@astrojs/preact";
 
 // https://astro.build/config
